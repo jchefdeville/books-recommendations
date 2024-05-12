2183ad972ff7dbdc16d726c3f0879969b87ebe5f
unknown <jeremy.chefdeville@gmail.com>
2024-05-12 20:52:05 +0200

    Read ratings.csv  print average rating for a book

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # books-recommandation
-Books recommandation in Python using books-dataset from user saurabhbagchi website
+Books recommandation in Python using books-dataset from user saurabhbagchi on Kaggle website
+
+Open for contribution, but let me finish first what I'm writing :)
--- main.py
+++ main.py
@@ -3,12 +3,26 @@ import pandas as pd
 def readBookInformations():
     booksCsv = "source/books.csv"
     cols_to_read = ["ISBN", "Book-Title"]
-    donnees = pd.read_csv(booksCsv, usecols=cols_to_read, encoding='ISO-8859-1', delimiter=';')
-    return donnees
+    return pd.read_csv(booksCsv, usecols=cols_to_read, encoding='ISO-8859-1', delimiter=';')
 
-books = readBookInformations()
+def readRatings(isbn):
+    path_ratings = "source/ratings.csv"
+    ratings = pd.read_csv(path_ratings, encoding='ISO-8859-1', delimiter=';')
+    filterISBN = ratings["ISBN"] == isbn
+    filteredRatings = ratings[filterISBN]
+    filterNoVote = filteredRatings["Book-Rating"] > 0
+    return filteredRatings[filterNoVote]
 
-filtre = books["ISBN"] == '0002005018'
-donnees_filtrees = books[filtre]
+bookHarryPotter1 = '059035342X'
 
-print(donnees_filtrees)
+# Read books
+dfBooks = readBookInformations()
+
+filterISBN = dfBooks["ISBN"] == bookHarryPotter1
+dfBooks = dfBooks[filterISBN]
+print(dfBooks)
+
+# Ratings > 0 from book
+dfRatings = readRatings(bookHarryPotter1)
+
+print("Average rating =", + dfRatings["Book-Rating"].mean(), "for", len(dfRatings), "users")
