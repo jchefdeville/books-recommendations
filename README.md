75c87172907a977f8d67466fe6c2bfad1f9cc86e
unknown <jeremy.chefdeville@gmail.com>
2024-05-13 15:48:35 +0200

    Clean old functions from previous csv files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # books-recommandation
 Books recommandation in Python using Amazon Books Dataset from Kaggle website
 
+
 Open for contribution, but let me finish first what I'm writing :)
+
 I probably won't do the frontend part, feel free to do it !
 
 ```
--- main.py
+++ main.py
@@ -5,77 +5,32 @@ def getBooks():
     booksCsv = "datas/amazon_books.csv"
     return pd.read_csv(booksCsv, encoding='ISO-8859-1', delimiter=',')
 
-def getAuthorBooks(dfBooks, author):
-    author = "['Neal Shusterman']"
-    filterISBN = dfBooks["authors"] == author
-    return dfBooks[filterISBN].sort_values(by='publishedDate')
-
 def getRatings():
     booksCsv = "datas/amazon_ratings.csv"
     return pd.read_csv(booksCsv, encoding='ISO-8859-1', delimiter=',')
 
-# dfBooks = getBooksGoodread()
+def getAuthorBooks(dfBooks, author):
+    authors = "['Neal Shusterman']"
+    print("Filter by authors" + authors)
+    filterAuthors = dfBooks["authors"] == authors
+    return dfBooks[filterAuthors].sort_values(by='publishedDate')
+
+def getBookRatings(dfRatings):
+    bookName = "Dark Matter"
+    print("Filter by bookName" + bookName)
+    filterTitle = dfRatings["Title"] == bookName
+    return dfRatings[filterTitle]
+
+
+# MAIN CODE #
+dfBooks = getBooks()
 # print(dfBooks.iloc[-1])
 
-# dfAuthorBooks = getAuthorBooks(dfBooks, "")
-# print(dfAuthorBooks)
+dfAuthorBooks = getAuthorBooks(dfBooks, "")
+print(dfAuthorBooks)
 
+dfRatings = getRatings()
+print(dfRatings.iloc[-1])
 
-# To Reactivate. Think of a cache solution => Pickle or Feather
-# dfBooks = getRatingGoodread()
-
-
-# OLD FUNCTIONS
-# Read CSV functions #
-def getBookInformations(isbn):
-    booksCsv = "source/books.csv"
-    cols_to_read = ["ISBN", "Book-Title"]
-    dfBooks = pd.read_csv(booksCsv, usecols=cols_to_read, encoding='ISO-8859-1', delimiter=';')
-    filterISBN = dfBooks["ISBN"] == isbn
-    return dfBooks[filterISBN]
-
-def getBooks(author):
-    booksCsv = "source/books.csv"
-    cols_to_read = ["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication"]
-    dfBooks = pd.read_csv(booksCsv, usecols=cols_to_read, encoding='ISO-8859-1', delimiter=';')
-    filterISBN = dfBooks["Book-Author"] == author
-    return dfBooks[filterISBN].sort_values(by='Year-Of-Publication')
-
-def getRatingsAuthor(dfBooks):
-    for isbn in dfBooks["ISBN"].values:
-        printBookRatings(isbn)
-
-# def getRatings(isbn):
-    path_ratings = "source/ratings.csv"
-    ratings = pd.read_csv(path_ratings, encoding='ISO-8859-1', delimiter=';')
-    filterISBN = ratings["ISBN"] == isbn
-    filteredRatings = ratings[filterISBN]
-    filterNoVote = filteredRatings["Book-Rating"] > 0
-    return filteredRatings[filterNoVote]
-
-
-# Prints functions #
-def printBookRatings(isbn):
-    dfBook = getBookInformations(isbn)
-    print(dfBook["Book-Title"].values)
-
-    dfRatings = getRatings(isbn)
-    print("Average rating =", + dfRatings["Book-Rating"].mean(), "for", len(dfRatings), "users")
-
-
-# Execution main code #
-author = "Neal Shusterman"
-# dfBooks = getBooks(author)
-# print(dfBooks)
-
-# getRatingsAuthor(dfBooks)
-
-print("******")
-
-isbnHarryPotter1 = '059035342X'
-# printBookRatings(isbnHarryPotter1)
-
-print("******")
-
-isbnHarryPotter2 = '0439064872'
-# printBookRatings(isbnHarryPotter2)
+dfBookRatings = getBookRatings(dfRatings)
+print(dfBookRatings)
