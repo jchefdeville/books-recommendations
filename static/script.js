9af04ab3afdb0b31ab5c675edcb79f279973358a
unknown <jeremy.chefdeville@gmail.com>
2024-05-19 18:10:14 +0200

    Add menu burger for navigation between routes

--- static/script.js
+++ static/script.js
@@ -13,3 +13,18 @@ function toggleDescription(index) {
         moreText.style.display = "inline";
     }
 }
+
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+    sidenav.classList.add("active");
+}
+
+function closeNav() {
+    sidenav.classList.remove("active");
+}
